a69649a7211c1287099ad8f87ace8476eedb2a70
Thamiris Calixto <thamiris.calixto@live.com>
2020-08-01 19:29:32 -0300

    Aula 04 Finalizada - Ajustes Heroku Host

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,7 +32,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     console.log('OI');
-    const URL ='http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'https://catfliix.herokuapp.com/categorias';
     fetch (URL)
     .then(async (respostaDoServidor) => {
       const resposta = await respostaDoServidor.json ();
